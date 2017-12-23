e1fb04e54e6356993f2a0830bdd9e3908f8b3c0e
macefti276 <macefti276@gmail.com>
2017-12-23 20:01:45 +0600

    Product Data

---
+++ easylife/data/product_data_access.php
@@ -0,0 +1,45 @@
+<?php require_once "data_access.php"; ?>
+<?php
+	function addProductToDB($product)
+	{
+        $sql = "INSERT INTO product (Product_Code,Name,Quantity,Total_Sells,Price,Last_Sold,Image,Catagory,Brand,Size,Description) VALUES ('$product[Product_Code]', '$product[Name]', '$product[Quantity]', 0, '$product[Price]', NULL, '$product[Image]', '$product[Catagory]', '$product[Brand]', '$product[Size]', '$product[Description]')";
+        $result = executeSQL($sql);
+        return $result;
+    }
+	
+	function editProductToDb($product){
+        $sql = "UPDATE product SET Product_Code='$product[Product_Code]',Name='$product[Name]',Quantity='$product[Quantity]',Total_Sells='$product[Total_Sells]',Price='$product[Price]',Last_Sold='$product[Last_Sold]',Image='$product[Image]',Catagory='$product[Catagory]',Brand='$product[Brand]',Size='$product[Size]',Description='$product[Description]'";
+        $result = executeSQL($sql);
+        return $result;
+    }
+	
+	function deleteProductFromDB($product)
+	{
+		$sql = "DELETE FROM product WHERE Product_Code='$product[Product_Code]'";        
+        $result = executeSQL($sql);
+        return $result;
+	}
+	
+	function getAllProductsFromDB(){
+        $sql = "SELECT * FROM product";        
+        $result = executeSQL($sql);
+        
+        $products = array();
+        for($i=0; $row=mysqli_fetch_assoc($result); ++$i){
+            $products[$i] = $row;
+        }
+        
+        return $products;
+    }
+	
+	function getProductByCodeFromDB($Product_code){
+        $sql = "SELECT * FROM product WHERE Product_code=$$Product_code";        
+        $result = executeSQL($sql);
+        
+        $product = mysqli_fetch_assoc($result);
+        
+        return $product;
+    }
+	
+	
+	
