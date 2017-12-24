ced2f575b3f77eafd3bbfe7138a01596ec7a263c
macefti276 <macefti276@gmail.com>
2017-12-25 00:37:06 +0600

    Add Member almost complete

--- easylife/app/admin/a_success.php
+++ easylife/app/admin/a_success.php
@@ -4,5 +4,4 @@
 <html>
 	<h3>Successful</h3>
 	<iframe frameborder="0" width="100%" height="380" src="uview.php?memberID=<?=$memberID?>"></iframe>
-	<a href="tadmindetails.php"><button>Back</button></a>
 </html>
